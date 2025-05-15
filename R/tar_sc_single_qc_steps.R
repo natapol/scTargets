25dd984a57c4d223cdb9ec90ec819ff8ff712db8
Natapol Pornputtapong <2310273+natapol@users.noreply.github.com>
2025-05-15 12:31:03 +0200

    for debug

--- R/tar_sc_single_qc_steps.R
+++ R/tar_sc_single_qc_steps.R
@@ -328,7 +328,7 @@ tar_sc_single_qc_step_apply_filter <- function(sce_sensitive_filter, sce_custom_
     sce_filter <- sce_custom_filter
   }
 
-  # num_cells <- min_ratio_cells * ncol(sce_filter)
+  num_cells <- min_ratio_cells * ncol(sce_filter)
   # sce_gene_filter <- !rowSums(counts(sce_filter) >= min_umi) >= num_cells
 
   # sce_filter[!sce_gene_filter, ]
